0f05259299bcffc692817096f9a59d207c8efbed
WebPrims <webprims@gmail.com>
2026-08-21 15:49:33 +0530

    Add ESP32 Wi-Fi connection example

---
+++ 04-wifi-connection/wifi_connection.ino
@@ -0,0 +1,22 @@
+#include <WiFi.h>
+
+const char* WIFI_SSID = "YOUR_WIFI_NAME";
+const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
