478e241b2a822014b91615a0b273bb75ff90cf9b
WebPrims <webprims@gmail.com>
2026-08-21 15:49:57 +0530

    Add DHT temperature humidity monitor

---
+++ 07-dht-monitor/dht_monitor.ino
@@ -0,0 +1,30 @@
+#include <DHT.h>
+
+#define DHT_PIN 4
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °C | Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
