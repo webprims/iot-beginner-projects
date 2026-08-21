261098bcb6b7a8b1a595955c289717f4a9be7316
WebPrims <webprims@gmail.com>
2026-08-21 15:49:12 +0530

    Add ESP32 LED blink example

---
+++ 01-led-blink/led_blink.ino
@@ -0,0 +1,12 @@
+const int LED_PIN = 2;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
